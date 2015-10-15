2c8373bfbdbb230d07e2ec40d3b834a3b0ec29e4
aizu04 <alexizucarmona@gmail.com>
2015-10-15 12:45:14 +0200

    Ya va el test01

--- src/GSILib/Misc/SSTest01.java
+++ src/GSILib/Misc/SSTest01.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import javax.swing.table.*;
 import org.jopendocument.dom.OOUtils;
+import org.jopendocument.dom.spreadsheet.Sheet;
 import org.jopendocument.dom.spreadsheet.SpreadSheet;
 
 /**
@@ -19,26 +20,31 @@ public class SSTest01 {
     
     public static void main(String[] args) throws IOException{
         
-        DefaultTableModel table = new DefaultTableModel(4,6);
-        int cont = 1;
-        // Rellenamos el array bidimensional con valores numericos
-        // para ello uso una variable int contador que ira aumentado
-        // cada vez que inserte un numero en la matriz
-        for(int i=0;i<4;i++){
-            for(int j=0;j<6;j++){
-                table.setValueAt(cont,i,j);
-                cont++;
-            }
-        }
+        DefaultTableModel table = new DefaultTableModel(4,6);       
         final File file = new File("test01.ods");
         try{
+            SpreadSheet.createEmpty(table);
             SpreadSheet.createEmpty(table).saveAs(file);
         }
         catch (IOException e){
             System.out.println("An error with the IO system appear");
         }
         
-        OOUtils.open(file);
+        Sheet sheet;
+        sheet = SpreadSheet.createFromFile(file).getSheet(0);
+        
+        int cont = 1;
+        // Rellenamos el array bidimensional con valores numericos
+        // para ello uso una variable int contador que ira aumentado
+        // cada vez que inserte un numero en la matriz
+        for(int i=0;i<4;i++){
+            for(int j=0;j<6;j++){
+                sheet.setValueAt(cont, j, i);
+                cont++;
+            }
+        }
+        
+        OOUtils.open(sheet.getSpreadSheet().saveAs(file));
 
     }
     
