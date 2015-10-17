85ce30b8e2b320e0af49ce79ceb6ca0d90b5d252
MiryamSubiza <miryam_247@hotmail.com>
2015-10-17 16:24:49 +0200

    Finalizada clase SSTest03.java

--- src/GSILib/Misc/SSTest03.java
+++ src/GSILib/Misc/SSTest03.java
@@ -1,24 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Gestion de Sistemas de Informacion
+ * Universidad Publica de Navarra
+ * First semester of the Academic Year 2015-2016
  */
 
 package GSILib.Misc;
 
-import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
-import javax.swing.table.DefaultTableModel;
-import org.jopendocument.dom.OOUtils;
-import org.jopendocument.dom.spreadsheet.MutableCell;
 import org.jopendocument.dom.spreadsheet.Sheet;
 import org.jopendocument.dom.spreadsheet.SpreadSheet;
 
 /**
- *
- * @author labora1
+ * Lectura de datos almacenados en el archivo test02.ods. El color es 
+ * innecesario, pero los datos numéricos han de recuperarse.
+ * @author subiza.79082
+ * @author izu.78236
+ * @version 15/10/2015
  */
+
 public class SSTest03 {
     
     public static void main(String[] args) throws IOException {
@@ -31,10 +31,14 @@ public class SSTest03 {
         Sheet sheet;
         sheet = SpreadSheet.createFromFile(file).getSheet(0);
         
+        
+        /* La posición donde se encuentra la matriz en la hoja de cálculo 
+         * es codificada de manera explícita (hard-coded).
+         */
         for (int i = 5; i < 9; i++) {
             for (int j = 3; j < 9; j++) {
                 valores[i-5][j-3] = (java.math.BigDecimal)sheet.getCellAt(j, i).getValue();
-                System.out.print(valores[i-5][j-3] + " ");
+                System.out.print(valores[i-5][j-3] + "\t");
             }
             System.out.println();
         }
