c69186d1c5a857009db967255469c59c01e777f3
aizu04 <alexizucarmona@gmail.com>
2015-10-22 12:35:31 +0200

    He cambiado el nombre del paquete porque lo ponia en el enunciado de la practica que lo habia puesto Carlos mal

---
+++ src/GSILabs/Misc/SSTest05.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GSILabs.Misc;
+
+import GSILabs.BSystem.BussinessSystem;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Alex
+ */
+public class SSTest05 {
+    
+    private static BussinessSystem bSystem;
+    
+    public static void main(String[] args) throws IOException{
+        
+        final File file = new File("P02Ej05.ods");
+        bSystem = new BussinessSystem();
+        int ticketsOk = bSystem.importTickets(file);
+        System.out.println("El numero de tickets introducidos: "+ ticketsOk);
+        
+    }
+}
