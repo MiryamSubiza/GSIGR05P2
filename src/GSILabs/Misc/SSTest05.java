7ba17ac7491c9da07f583e93c75528fbbfe710d1
aizu04 <alexizucarmona@gmail.com>
2015-10-29 12:57:28 +0100

    Añado un evento para comprobar si añade bien los tickets

--- src/GSILabs/Misc/SSTest05.java
+++ src/GSILabs/Misc/SSTest05.java
@@ -5,6 +5,7 @@
  */
 package GSILabs.Misc;
 
+import GSILabs.BModel.*;
 import GSILabs.BSystem.BussinessSystem;
 import java.io.File;
 import java.io.IOException;
@@ -20,7 +21,16 @@ public class SSTest05 {
     public static void main(String[] args) throws IOException{
         
         final File file = new File("P05Ej02.ods");
-        bSystem = new BussinessSystem();        
+        bSystem = new BussinessSystem();
+        
+        Location l = new Location("Palacio Mestalla",10000,"Valencia");
+        Artist a = new Artist("ACDC", "Best Rock Band Ever", "www.acdc.com");
+        bSystem.addLocation(l);
+        bSystem.addArtist(a);
+        Concert c = new Concert("ACDC en VAL 2015", a, new FechaCompleta("02/02/2015","22:00"), new FechaCompleta("02/02/2015","22:00"),
+                    new FechaCompleta("02/02/2015","21:00"), new FechaCompleta("03/02/2015","00:00"), l);
+        bSystem.addNewConcert(c);
+        
         int ticketsOk = bSystem.importTickets(file);
         System.out.println("El numero de tickets introducidos: "+ ticketsOk);
         
